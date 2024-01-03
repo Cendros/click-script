704aeea1959ac27aa24f2dfaaef3a42b500820ce
Goramax <maximemalherbe5@gmail.com>
2024-01-03 16:28:32 +0100

    converted makeScripts to Object and show in TreeView

--- src/extension.ts
+++ src/extension.ts
@@ -6,7 +6,7 @@ import {
   Event,
   ThemeIcon,
 } from "vscode";
-import { executeScript, getMakeScritps, getScripts } from "./utils";
+import { executeScript, getMakeScripts, getScripts } from "./utils";
 
 export function activate(context: ExtensionContext) {
   const disposable = commands.registerCommand(
@@ -26,7 +26,7 @@ export function activate(context: ExtensionContext) {
 
       quickPick.onDidAccept(() => {
         const { label } = quickPick.activeItems[0];
-        executeScript('npm run', label);
+        executeScript("npm run", label);
         quickPick.hide();
       });
 
@@ -37,21 +37,19 @@ export function activate(context: ExtensionContext) {
   const disposableMake = commands.registerCommand(
     "click-script.executeMake",
     async () => {
-      const scripts = getMakeScritps();
+      const scripts = getMakeScripts();
       if (!scripts) {
         return window.showErrorMessage(
           "You don't have any scripts in your Makefile file."
         );
       }
       const quickPick = window.createQuickPick();
-      quickPick.items = scripts.map(
-        (label: string) => ({ label: label })
-      );
+      quickPick.items = scripts.scripts.map((label: string) => ({ label: label }));
       quickPick.title = "Choose a script to execute";
 
       quickPick.onDidAccept(() => {
         const { label } = quickPick.activeItems[0];
-        executeScript('make', label);
+        executeScript("make", label);
         quickPick.hide();
       });
 
@@ -63,7 +61,7 @@ export function activate(context: ExtensionContext) {
     new EventEmitter<undefined>();
   const onDidChangeTreeData: Event<undefined> = _onDidChangeTreeData.event;
 
-  const treeView = window.createTreeView("click-script-btns", {
+  const packageTreeView = window.createTreeView("click-script-package", {
     treeDataProvider: {
       getChildren: () => {
         const scripts = getScripts();
@@ -86,15 +84,46 @@ export function activate(context: ExtensionContext) {
     },
   });
 
-  treeView.onDidChangeSelection((item) => {
-    executeScript('npm run', item.selection[0].label);
+  packageTreeView.onDidChangeSelection((item) => {
+    executeScript("npm run", item.selection[0].label);
   });
 
-  treeView.onDidChangeVisibility(() => {
+  packageTreeView.onDidChangeVisibility(() => {
     _onDidChangeTreeData.fire(undefined);
   });
 
-  context.subscriptions.push(disposable, disposableMake, treeView);
+  const makeTreeView = window.createTreeView("click-script-make", {
+    treeDataProvider: {
+      getChildren: () => {
+        const scripts = getMakeScripts();
+        if (!scripts)
+          window.showErrorMessage(
+            "You don't have any scripts in your Makefile."
+          );
+
+        return scripts
+          ? Object.keys(scripts.scripts).map((label) => ({
+              label: label,
+              iconPath: new ThemeIcon("play"),
+            }))
+          : [];
+      },
+      getTreeItem: (items) => {
+        return items;
+      },
+      onDidChangeTreeData: onDidChangeTreeData,
+    },
+  });
+
+  makeTreeView.onDidChangeSelection((item) => {
+    executeScript("make", item.selection[0].label);
+  });
+
+  makeTreeView.onDidChangeVisibility(() => {
+    _onDidChangeTreeData.fire(undefined);
+  });
+
+  context.subscriptions.push(disposable, disposableMake, packageTreeView, makeTreeView);
 }
 
 export function deactivate() {}
--- src/utils.ts
+++ src/utils.ts
@@ -1,48 +1,56 @@
-import * as vscode from 'vscode';
-import * as fs from 'fs';
+import * as vscode from "vscode";
+import * as fs from "fs";
 
 export const getScripts = () => {
-    if (!vscode.workspace.workspaceFolders) {
-        vscode.window.showErrorMessage('Unable to locate files.');
-        return;
-    }
-    const f = vscode.workspace.workspaceFolders[0].uri.fsPath;
+  if (!vscode.workspace.workspaceFolders) {
+    vscode.window.showErrorMessage("Unable to locate files.");
+    return;
+  }
+  const f = vscode.workspace.workspaceFolders[0].uri.fsPath;
 
-    let file;
-    try {
-        file = fs.readFileSync(`${f}/package.json`, { encoding: 'utf8' });
-    } catch (err) {
-        vscode.window.showErrorMessage('Unable to find package.json.');
-        return;
-    }
-    const { scripts } = JSON.parse(file.toString());
+  let file;
+  try {
+    file = fs.readFileSync(`${f}/package.json`, { encoding: "utf8" });
+  } catch (err) {
+    vscode.window.showErrorMessage("Unable to find package.json.");
+    return;
+  }
+  const { scripts } = JSON.parse(file.toString());
 
-    return { scripts };
+  console.log("scripts package", scripts);
+
+  return { scripts };
 };
 
-export const getMakeScritps = () => {
-    if (!vscode.workspace.workspaceFolders) {
-        vscode.window.showErrorMessage('Unable to locate files.');
-        return;
-    }
+export const getMakeScripts = () => {
+    console.log("getMakeScripts");
+  if (!vscode.workspace.workspaceFolders) {
+    vscode.window.showErrorMessage("Unable to locate files.");
+    return;
+  }
 
-    const f = vscode.workspace.workspaceFolders[0].uri.fsPath;
+  const f = vscode.workspace.workspaceFolders[0].uri.fsPath;
 
-    let file;
-    try {
-        file = fs.readFileSync(`${f}/Makefile`, { encoding: 'utf8' });
-    } catch (err) {
-        vscode.window.showErrorMessage('Unable to find Makefile.');
-        return;
-    }
-    const content = file.toString();
-    const scripts = content.match(/^.*:$/gm)?.map(res => res.slice(0, -1)) ?? [];
+  let file;
+  try {
+    file = fs.readFileSync(`${f}/Makefile`, { encoding: "utf8" });
+  } catch (err) {
+    vscode.window.showErrorMessage("Unable to find Makefile.");
+    return;
+  }
+  const content = file.toString();
+  let scripts = content.match(/^.*:$/gm)?.map((res) => res.slice(0, -1)) ?? [] as any[];
 
-    return scripts;
-}
+    scripts = scripts.reduce((acc, cur) => {
+        acc[cur] = cur;
+        return acc;
+    }, {} as any);
+
+  return { scripts };
+};
 
 export const executeScript = (executer: string, name: string) => {
-    const terminal = vscode.window.createTerminal(name);
-    terminal.show();
-    terminal.sendText(`${executer} ${name}`);
+  const terminal = vscode.window.createTerminal(name);
+  terminal.show();
+  terminal.sendText(`${executer} ${name}`);
 };
